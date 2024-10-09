5f7eb1b0b7671d9dcc0f178b27e55a182b9ef06a
Mingyang Liu <castielliu525@gmail.com>
2024-10-09 19:07:50 +0000

    added Order model

---
+++ service/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+Models for order
+
+All of the models are stored in this package
+"""
+
+from .persistent_base import db, DataValidationError
+from .order import Order
+
+# from .item import Item
--- tests/factories.py
+++ tests/factories.py
@@ -3,18 +3,24 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from service.models import Order
+from factory.fuzzy import FuzzyDate, FuzzyInteger, FuzzyDecimal
+from datetime import date
 
 
-class YourResourceModelFactory(factory.Factory):
-    """Creates fake pets that you don't have to feed"""
+class OrderFactory(factory.Factory):
+    """Creates fake Order that you don't have to pay"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = YourResourceModel
+        model = Order
 
     id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
+    date = FuzzyDate(date(2008, 1, 1))
+    status = FuzzyInteger(0, 2)
+    amount = FuzzyDecimal(1.0)
+    address = factory.Faker("address")
+    customer_id = factory.Sequence(lambda n: n)
 
     # Todo: Add your other attributes here...
