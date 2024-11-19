9866875df039cb3a1e4c8aa569d620af5140c777
TianlongZhu <zt634@nyu.edu>
2024-11-19 13:19:41 -0500

    fix lint

--- service/routes.py
+++ service/routes.py
@@ -28,6 +28,7 @@ from service.models import Order
 from service.models import Item
 from service.common import status  # HTTP Status Codes
 
+
 ######################################################################
 # HEALTH CHECK
 ######################################################################
@@ -35,6 +36,7 @@ from service.common import status  # HTTP Status Codes
 def health_check():
     """Let them know our heart is still beating"""
     return jsonify(status=200, message="Healthy"), status.HTTP_200_OK
+
 
 ######################################################################
 # GET INDEX
